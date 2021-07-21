16b12c0385dff10039d8aab77dc07ea449a2ec20
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-21 07:08:03 +0530

    Show notes in one category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_categories, only: %i[index show]
 
   # GET /categories or /categories.json
   def index
@@ -8,6 +9,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
+    @notes = @category.notes
   end
 
   # GET /categories/new
@@ -65,5 +67,9 @@ class CategoriesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def set_categories
+      @categories = Category.all
     end
 end
