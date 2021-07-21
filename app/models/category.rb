f86d6654269c9c9718403ad5665d32abf2813f04
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-21 07:38:30 +0530

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     has_and_belongs_to_many :notes
+    validates :name, presence: true
 
     def to_s
         name
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
     has_and_belongs_to_many :categories
+    validates :content, presence: true
 end
